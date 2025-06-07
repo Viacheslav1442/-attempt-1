63af94aba4b25019a96101d35d13e0205827fdd9
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-06-07 11:50:58 +0300

    js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-// main.js
-
-// Імпортуємо функцію ініціалізації секції артистів
 import { initArtistSection } from './artist-section.js';
 
 // Чекаємо, доки DOM буде повністю завантажений, перш ніж ініціалізувати додаток
 document.addEventListener('DOMContentLoaded', () => {
     initArtistSection();
 });
+
