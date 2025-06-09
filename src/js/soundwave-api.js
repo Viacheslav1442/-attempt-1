1a65fd95e54695c74936d0b4f6a05caef227580d
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-06-09 14:10:30 +0300

    js

--- src/js/soundwave-api.js
+++ src/js/soundwave-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sound-wave.b.goit.study/api',
+  baseURL: 'https://sound-wave.b.goit.study/api-docs/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
