1a980db3fba5eb89197fd793d76e22f6e78edb61
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-06-09 13:28:58 +0300

    js

--- src/js/soundwave-api.js
+++ src/js/soundwave-api.js
@@ -11,15 +11,6 @@ const api = axios.create({
 });
 
 
-function showLoader() {
-  const loader = document.getElementById('loader');
-  if (loader) loader.style.display = 'block';
-}
-
-function hideLoader() {
-  const loader = document.getElementById('loader');
-  if (loader) loader.style.display = 'none';
-}
 
 // ➕ Interceptors — показ лоадера перед запитом
 api.interceptors.request.use(
